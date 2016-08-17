5ca483baad6f10029037d4d83a83ae4f6c67b9af
Régis Behmo <regis@behmo.com>
2016-08-17 09:03:56 +0200

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_requirements():
 
 setup(
         name='spotify-onthego',
-        version='0.0.3',
+        version='0.0.4',
         description="Download Spotify playlists via YouTube",
         long_description=get_long_description(),
         classifiers=[
