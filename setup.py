ab6d8929024f13f1787998e161a27a8011e00a58
Régis Behmo <regis@behmo.com>
2020-05-21 10:38:17 +0200

    Release v1.0.9

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def get_requirements():
 
 setup(
     name="spotify-onthego",
-    version="1.0.8",
+    version="1.0.9",
     description="Download Spotify songs and playlists (with YouTube)",
     long_description=get_long_description(),
     classifiers=[
