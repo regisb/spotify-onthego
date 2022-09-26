c6438f28da0d2a970feb24662e3e457ce2d907cb
Régis Behmo <regis@behmo.com>
2022-09-26 09:04:19 +0200

    chore: upgrade python

--- setup.py
+++ setup.py
@@ -35,9 +35,10 @@ setup(
     long_description=get_long_description(),
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Development Status :: 5 - Production/Stable",
         "Topic :: Multimedia :: Sound/Audio :: Capture/Recording",
